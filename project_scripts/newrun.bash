b9fad4804d44877900d91ac595af05b8906a989c
Christopher Brown <chris.brown@fpsl.net>
2018-11-28 14:28:11 +0000

    up to date bash script

--- project_scripts/newrun.bash
+++ project_scripts/newrun.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
-source /home/chris/root/builddir/bin/thisroot.sh
-source /home/chris/Documents/GEANT4/geant4.10.04-install/bin/geant4.sh
+source /cvmfs/sft.cern.ch/lcg/releases/ROOT/6.14.04-fe6f8/x86_64-slc6-gcc7-opt/bin/thisroot.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-slc6-gcc7-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/releases/Geant4/10.04.p02-fd180/x86_64-slc6-gcc7-opt/bin/geant4.sh
 
-cd /home/chris/Documents/Project/Simulation
+cd Simulation
+mkdir build/runs
 cp tchain.cxx build/runs
 cp positions.txt build/runs
 cp tfriends.cxx build/runs
@@ -26,21 +28,21 @@ do
 	root -b -q tchain.cxx\($c,2\)
 	cp "$c"out.root ..
 	cd ..
-	rm -r run$c
-	rm run.mac
+	rm -rf run$c
+	rm -rf run.mac
 done
 y=$v-1
 root -b -q tfriends.cxx\($y\)
 
 for (( c=1; c<$v; c++)) 
 do
-	rm "$c"out.root
+	rm -rf "$c"out.root
 	
 done
 
-rm tchain.cxx
-rm tfriends.cxx
-rm run.mac
-rm textgenerator.py
-rm positions.txt
-rm LXe
+rm -rf tchain.cxx
+rm -rf tfriends.cxx
+rm -rf run.mac
+rm -rf textgenerator.py
+rm -rf positions.txt
+rm -rf LXe
