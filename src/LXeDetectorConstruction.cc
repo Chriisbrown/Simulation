79a92e4347865d5d663c9ceae68e8d0e4d3f8074
Chriisbrown <41648127+Chriisbrown@users.noreply.github.com>
2018-12-11 16:41:00 +0000

    Update LXeDetectorConstruction.cc

--- src/LXeDetectorConstruction.cc
+++ src/LXeDetectorConstruction.cc
@@ -451,11 +451,6 @@ void LXeDetectorConstruction::SetDefaults() {
   fScint_x = 5.0*cm;
   fScint_y = 1*cm;
   fScint_z = 50.0*cm;
-=======
-  fScint_x = 20.0*cm;
-  fScint_y = 1.0*cm;
-  fScint_z = 20.0*cm;
->>>>>>> 06d3cf087ac464cac494965787bf6c4f5e024534
 
   fNx = 1;
   fNy = 1;
