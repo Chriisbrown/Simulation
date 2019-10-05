986114370eb180475657eddb3649987bb038c059
Christopher Brown <cb719@cb719-laptop.hep.ph.ic.ac.uk>
2019-10-05 20:51:05 +0100

    initial commit

---
+++ build/CMakeFiles/LXe.dir/DependInfo.cmake
@@ -0,0 +1,64 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/LXe.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/LXe.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXeActionInitialization.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXeActionInitialization.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXeDetectorConstruction.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXeDetectorConstruction.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXeDetectorMessenger.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXeDetectorMessenger.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXeEMPhysics.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXeEMPhysics.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXeEventAction.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXeEventAction.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXeEventMessenger.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXeEventMessenger.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXeGeneralPhysics.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXeGeneralPhysics.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXeMainVolume.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXeMainVolume.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXeMuonPhysics.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXeMuonPhysics.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXePMTHit.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXePMTHit.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXePMTSD.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXePMTSD.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXePhysicsList.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXePhysicsList.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXePrimaryGeneratorAction.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXePrimaryGeneratorAction.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXeRunAction.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXeRunAction.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXeRunMessenger.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXeRunMessenger.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXeScintHit.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXeScintHit.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXeScintSD.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXeScintSD.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXeStackingAction.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXeStackingAction.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXeSteppingAction.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXeSteppingAction.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXeSteppingMessenger.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXeSteppingMessenger.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXeSteppingVerbose.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXeSteppingVerbose.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXeTrackingAction.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXeTrackingAction.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXeTrajectory.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXeTrajectory.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXeUserEventInformation.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXeUserEventInformation.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXeUserTrackInformation.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXeUserTrackInformation.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXeWLSFiber.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXeWLSFiber.cc.o"
+  "/home/cb719/Documents/GEANT4/Programs/Simulation/src/LXeWLSSlab.cc" "/home/cb719/Documents/GEANT4/Programs/Simulation/build/CMakeFiles/LXe.dir/src/LXeWLSSlab.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4MULTITHREADED"
+  "G4UI_USE"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_RAYTRACERX"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/cb719/Documents/GEANT4/geant4.10.05-install/include/Geant4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/chris/Documents/Documents/Physics/Fourth Year/Porject/Results - Full/imperfect/Simulation
+# Install script for directory: /home/cb719/Documents/GEANT4/Programs/Simulation
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/LXe")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -27,20 +27,33 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "0")
+endif()
+
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/Users/chris/Documents/Documents/Physics/Fourth Year/Porject/Results - Full/imperfect/Simulation/build/Debug/LXe.exe")
-  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/Users/chris/Documents/Documents/Physics/Fourth Year/Porject/Results - Full/imperfect/Simulation/build/Release/LXe.exe")
-  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/Users/chris/Documents/Documents/Physics/Fourth Year/Porject/Results - Full/imperfect/Simulation/build/MinSizeRel/LXe.exe")
-  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/Users/chris/Documents/Documents/Physics/Fourth Year/Porject/Results - Full/imperfect/Simulation/build/RelWithDebInfo/LXe.exe")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/LXe" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/LXe")
+    file(RPATH_CHECK
+         FILE "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/LXe"
+         RPATH "")
+  endif()
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "/home/cb719/Documents/GEANT4/Programs/Simulation/build/LXe")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/LXe" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/LXe")
+    file(RPATH_CHANGE
+         FILE "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/LXe"
+         OLD_RPATH "/home/cb719/Documents/GEANT4/geant4.10.05-install/lib64:"
+         NEW_RPATH "")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/LXe")
+    endif()
   endif()
 endif()
 
@@ -52,5 +65,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/chris/Documents/Documents/Physics/Fourth Year/Porject/Results - Full/imperfect/Simulation/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/cb719/Documents/GEANT4/Programs/Simulation/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
